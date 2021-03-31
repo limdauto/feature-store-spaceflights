7d86bf54468748c3874398e81565fe93c1534acf
Lim Hoang <limdauto@gmail.com>
2021-03-31 15:44:51 +0100

    Use env var for bucket name

--- src/feature_store_spaceflights/hooks.py
+++ src/feature_store_spaceflights/hooks.py
@@ -27,9 +27,10 @@
 # limitations under the License.
 
 """Project hooks."""
+import os
 from typing import Any, Dict, Iterable, Optional
 
-from kedro.config import ConfigLoader
+from kedro.config import TemplatedConfigLoader
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog
 from kedro.versioning import Journal
@@ -39,8 +40,15 @@ class ProjectHooks:
     @hook_impl
     def register_config_loader(
         self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any]
-    ) -> ConfigLoader:
-        return ConfigLoader(conf_paths)
+    ) -> TemplatedConfigLoader:
+        return TemplatedConfigLoader(
+            conf_paths,
+            globals_dict={
+                "AWS_S3_FEATURE_STORE_BUCKET": os.environ.get(
+                    "AWS_S3_FEATURE_STORE_BUCKET"
+                ),
+            },
+        )
 
     @hook_impl
     def register_catalog(
